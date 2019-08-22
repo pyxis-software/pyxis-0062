fad46743d1ad28106f3a68c1a3754cb0652ba3ce
Jefferson <jeffersonbds_barros@hotmail.com>
2019-08-22 13:18:17 -0300

    Menu principal

---
+++ _src/_components/Menu.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Image, Button, Text} from 'react-native';
+import {connect} from 'react-redux';
+import {Actions} from 'react-native-router-flux';
+
+const LogoJuniorNet = require('../_imagens/JuniorNET.png');
+
+class Menu extends Component{
+  render(){
+    return(
+      <View style={styles.containerPrincipal}>
+
+        <View style={styles.containerLogo}>
+          <Image source={LogoJuniorNet} style={styles.logo} />
+        </View>
+
+        <View style={styles.containerOpcoes1}>
+          <View style={styles.opcoes}>
+            <Text style={styles.textoOpcoes}> Meus Dados </Text>
+          </View>
+          <View style={styles.opcoes}>
+            <Text style={styles.textoOpcoes}> Financeiro</Text>
+          </View>
+          <View style={styles.opcoes}>
+            <Text style={styles.textoOpcoes}> Notícias </Text>
+          </View>
+        </View>
+
+        <View style={styles.containerOpcoes2}>
+          <View style={styles.opcoes}>
+            <Text style={styles.textoOpcoes}> Planos </Text>
+          </View>
+          <View style={styles.opcoes}>
+            <Text style={styles.textoOpcoes}> Chat </Text>
+          </View>
+          <View style={{width: 100, height: 100, opacity: 0, borderRadius: 10}}></View>
+        </View>
+
+        <Button title="Sair" onPress={() => {return false}} color="#3258A4" />
+
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+
+});
+
+const styles = StyleSheet.create({
+  containerPrincipal: {
+    flex: 1
+  },
+  containerLogo: {
+    flex: 2,
+    paddingTop: 30,
+    alignItems: 'center'
+  },
+  logo: {
+    width: 180,
+    height: 180
+  },
+  containerOpcoes1: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  opcoes: {
+    width: 100,
+    height: 100,
+    backgroundColor: '#3258A4',
+    borderRadius: 10
+  },
+  containerOpcoes2: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  textoOpcoes: {
+    color: '#fff',
+    textAlign: 'center'
+  }
+});
+
+export default connect(mapStateToProps, {})(Menu);
--- _src/_components/Routes.js
+++ _src/_components/Routes.js
@@ -3,10 +3,14 @@ import {Router, Scene, Stack} from 'react-native-router-flux';
 
 import TelaInicial from './TelaInicial';
 import EsqueciSenha from './EsqueciSenha';
+import Menu from './Menu';
 
 export default props => (
   <Router>
     <Stack key="root">
+
+      <Scene key="menu" component={Menu} hideNavBar={true} />
+
       <Scene key="inicial" component={TelaInicial} hideNavBar={true} />
       <Scene key="esqueciSenha" component={EsqueciSenha} hideNavBar={true} />
     </Stack>
